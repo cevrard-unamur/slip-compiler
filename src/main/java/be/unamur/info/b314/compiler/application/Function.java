d686d409000b28c597151ebae93f03ddbd10523f
Cedric Evrard <cedric@evrard.io>
2020-04-07 22:14:25 +0200

    add argument existance as variable

--- src/main/java/be/unamur/info/b314/compiler/application/Function.java
+++ src/main/java/be/unamur/info/b314/compiler/application/Function.java
@@ -1,12 +1,29 @@
 package be.unamur.info.b314.compiler.application;
 
+import be.unamur.info.b314.compiler.exception.VariableException;
+
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class Function extends Context {
+    private static final int defaultStackSize = 10;
+
     private String name;
     private String returnType;
 
-    private ArrayList<VariableBase> arguments;
+    private Hashtable<String, Integer> argumentSymbols;
+    private VariableBase[] arguments;
+    private int argumentHeapIndex = 0;
+
+    public Function(String name, String returnType, Context parent) {
+        this.argumentSymbols = new Hashtable<>();
+
+        this.arguments = new VariableBase[defaultStackSize];
+
+        this.name = name;
+        this.returnType = returnType;
+        super.parent = parent;
+    }
 
     public String getName() {
         return name;
@@ -14,29 +31,27 @@ public class Function extends Context {
 
     public String getReturnType() { return returnType; }
 
-    public int getSize()
-    {
-        return arguments.size();
-    }
+    public int getNumberOfArguments() { return argumentHeapIndex; }
 
-    public String getArgType(int i)
-    {
-        return arguments.get(i).getType();
-    }
+    public String getArgumentType(int i) { return arguments[i].getType(); }
 
-    public VariableBase getVar(int i)
-    {
-        return arguments.get(i);
-    }
+    public VariableBase getArgument(String name) { return this.arguments[this.argumentSymbols.get(name).intValue()]; }
 
     public void addArguments(ArrayList<VariableBase> arguments){
-        this.arguments = arguments;
+        for (VariableBase argument : arguments) {
+            addVariableBase(argument);
+        }
+    }
+    private void addVariableBase(VariableBase variable) {
+        if (!isExistingInSymbolTables(variable.getName())) {
+            this.argumentSymbols.put(variable.getName(), this.argumentHeapIndex);
+            this.arguments[argumentHeapIndex++] = variable;
+        } else {
+            throw new VariableException("An argument with the name " + variable.getName() + " already exist");
+        }
     }
 
-    public Function(String name, String returnType, Context parent) {
-        this.name = name;
-        this.returnType = returnType;
-        this.arguments = new ArrayList<>();
-        super.parent = parent;
+    private boolean isExistingInSymbolTables(String name) {
+        return this.variableSymbols.containsKey(name) || this.functionSymbols.containsKey(name);
     }
 }
