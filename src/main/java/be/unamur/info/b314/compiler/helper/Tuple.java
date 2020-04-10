cfe4fb0f2369d8ea116b848b3bf4a6e655d94a5a
Cedric Evrard <cedric@evrard.io>
2020-04-10 16:43:55 +0200

    Change Pair by Tuple

---
+++ src/main/java/be/unamur/info/b314/compiler/helper/Tuple.java
@@ -0,0 +1,11 @@
+package be.unamur.info.b314.compiler.helper;
+
+public class Tuple<X, Y> {
+    public final X Item1;
+    public final Y Item2;
+
+    public Tuple(X item1, Y item2) {
+        this.Item1 = item1;
+        this.Item2 = item2;
+    }
+}
