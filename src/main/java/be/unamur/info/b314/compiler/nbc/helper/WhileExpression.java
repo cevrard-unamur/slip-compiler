a639ff785d3272fe97b02024b6c5a0c72d983c5f
Cedric Evrard <cedric@evrard.io>
2020-05-03 17:29:10 +0200

    add for loop

--- src/main/java/be/unamur/info/b314/compiler/nbc/helper/WhileExpression.java
+++ src/main/java/be/unamur/info/b314/compiler/nbc/helper/WhileExpression.java
@@ -2,23 +2,48 @@ package be.unamur.info.b314.compiler.nbc.helper;
 
 import be.unamur.info.b314.compiler.PlayPlusParser;
 import be.unamur.info.b314.compiler.application.Application;
+import be.unamur.info.b314.compiler.nbc.writer.IfWriter;
+import be.unamur.info.b314.compiler.nbc.writer.NBCOpCodeTypes;
+import be.unamur.info.b314.compiler.nbc.writer.NBCWriter;
 
 import java.io.PrintWriter;
 
 public class WhileExpression {
+    private static int whileId = 1;
+    private static final String whileTemporaryVariable = "__whileVariable";
+
+    private static String getJumpName() { return WhileExpression.whileTemporaryVariable + WhileExpression.whileId; }
+    private static String getJumpNameEnd() { return WhileExpression.whileTemporaryVariable + WhileExpression.whileId + "End"; }
+
+
     public static void enterWhileInstruction(PlayPlusParser.WhileInstructionContext context, Application application, PrintWriter writer) {
         WhileExpression.enterFor((PlayPlusParser.WhileContext)context.whileBlock(), application, writer);
     }
 
     public static void enterFor(PlayPlusParser.WhileContext context, Application application, PrintWriter writer) {
-        // TODO : Handle the right expression
-        // TODO : Generate the NBC code for the beginning of the loop
-        //   - generate a label
-        //   - check if the values are equals,
-        //   - if yes, jump to the end
-        //   - otherwise, continue
-        // TODO : Execute the content of the loop (instruction)
-        // TODO : Generate the NBC code for the end of the loop
-        //   - otherwise, jump to the beginning
+        // Handle the right expression
+        String rightExpressionVariable = RightExpression.enterRightExpression(context.rightExpr(), application, writer);
+
+        // Write the label to restart the loop
+        NBCWriter.writeLabel(writer, WhileExpression.getJumpName());
+        // Write the compare condition
+        IfWriter.writeBreakIfCondition(
+                writer,
+                NBCOpCodeTypes.Equal,
+                WhileExpression.getJumpNameEnd(),
+                rightExpressionVariable,
+                NBCWriter.booleanTrueVariableName
+        );
+
+        // Content of the loop
+        for (PlayPlusParser.InstContext instruction : context.inst()) {
+            DeclarationExpression.enterInstruction(instruction, application, writer);
+        }
+
+        // Jump to the beginning of the loop
+        IfWriter.writeJump(writer, WhileExpression.getJumpName());
+        // End of the condition label
+        NBCWriter.writeLabel(writer, WhileExpression.getJumpNameEnd());
+        WhileExpression.whileId++;
     }
 }
