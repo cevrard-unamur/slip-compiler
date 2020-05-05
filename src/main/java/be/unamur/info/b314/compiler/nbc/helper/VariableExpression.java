fd4631a3d5c9ca2b504dd71160af55ac5dbd5f10
Matthias Nicolay <matthiasn.gaming@gmail.com>
2020-05-05 22:24:29 +0200

    doing things

--- src/main/java/be/unamur/info/b314/compiler/nbc/helper/VariableExpression.java
+++ src/main/java/be/unamur/info/b314/compiler/nbc/helper/VariableExpression.java
@@ -2,6 +2,9 @@ package be.unamur.info.b314.compiler.nbc.helper;
 
 import be.unamur.info.b314.compiler.PlayPlusParser;
 import be.unamur.info.b314.compiler.application.Application;
+import com.sun.org.apache.xerces.internal.dom.ChildNode;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 import java.io.PrintWriter;
 
@@ -12,6 +15,15 @@ public class VariableExpression {
         //    récupérer le resultat de l'expression droite et l'assigner à la variable existante
         //  Toutes les variables de l'application (définie dans le code SLIP) sont créés au début du programme NBC
         //  Créer des tests slip pour la fonctionnalité sur laquelle je travail
+
+        PlayPlusParser.GlobalDeclarationContext ctx = (PlayPlusParser.GlobalDeclarationContext)context;
+
+        ParseTree declarationType = ctx.getChild(0);
+
+        if (declarationType instanceof PlayPlusParser.VariableDeclarationContext){
+            PlayPlusParser.VariableDeclarationContext var = (PlayPlusParser.VariableDeclarationContext) declarationType;
+
+        }
     }
 
     public static void enterVariable(PlayPlusParser.VariableInstructionContext context, Application application, PrintWriter writer) {
